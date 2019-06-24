90c6eddbd29bc7540ad279250a28f9280faea5ea
yta-git <27941112+yta-git@users.noreply.github.com>
2019-06-25 00:21:58 +0900

    ????

--- digital_library.py
+++ digital_library.py
@@ -1,65 +1,105 @@
 import numpy as np
 from itertools import product
 from PIL import Image
+
 class filter:
-    def __init__(self, fsize): 
+    def __init__(self, window, sigma=1.3): 
+        fsize = 1 + 2 * window
+        print(f'using {fsize} * {fsize} filter')
         self.moving_average_filter = np.ones((fsize, fsize, 3), np.float) / fsize ** 2
-        self.ones_filter = np.ones((fsize, fsize, 3), np.float)
+
         self.high_pass_filter = np.zeros((fsize, fsize), np.float)
         self.high_pass_filter[:, 0] = -1
         self.high_pass_filter[:, -1] = 1
 
-class conv2D(filter):
-    def __init__(self, mat, window):
+        self.laplacian = np.zeros((fsize, fsize), np.float)
+        self.laplacian[fsize // 2, :] = 1/4
+        self.laplacian[:, fsize // 2] = 1/4
+        self.laplacian[fsize // 2, fsize // 2] = -1
+
+        self.gaussian = np.zeros((fsize, fsize), np.float)
+        for x, y in product(range(fsize), range(fsize)):
+            self.gaussian[y, x] = 1/(2*np.pi)**0.5 * np.exp(x**2 + y**2 / 2 / sigma**2)
+class filter2D:
+    def __init__(self, mat):
         self.mat = mat
-        self.window = window
-        self.target = target
-        super().__init__(1 + 2 * window)
-    
-    def extend_mat(self):
+        self.X, self.Y = mat.shape[-1]
+
+    def extend_mat(self, window):
         exmat = self.mat.copy()
+        exmat = np.concatenate(([exmat[0]] * window, exmat), axis=0)
+        exmat = np.concatenate((exmat, [exmat[-1]] * window), axis=0)
+        exmat = np.concatenate((np.array([exmat[:, 0]] * window).transpose(1, 0, 2), exmat), axis=1)
+        exmat = np.concatenate((exmat, np.array([exmat[:, -1]] * window).transpose(1, 0, 2)), axis=1)
+        return exmat
 
-    def moving_average(self, target):
-        
+    def conv(self, filter, target):
+        window = (filter.shape[0] - 1) / 2
+        tmpm = np.zeros_like(self.mat, np.float)
+        for x, y in product(range(window, self.X + window), range(window, self.Y + window)):
+            u, d, l, r = y - window, y + window + 1, x - window, x + window + 1
+            tmpm[y - window, x - window] = sum(sum(filter * self.extend_mat(window)[u:d, l:r]))
 
-
-
-    def moving_average(self, exmat)
-
-def conv(mat, mode, window, terget):
-    retmat = mat.copy()
-    exmat = mat.copy()
-    X, Y = mat.shape[:-1]
-    fsize = 1 + 2 * window
-    print(f'using {fsize} * {fsize} filter')
-
-    exmat = np.concatenate(([exmat[0]] * window, exmat), axis=0)
-    exmat = np.concatenate((exmat, [exmat[-1]] * window), axis=0)
-    exmat = np.concatenate((np.array([exmat[:, 0]] * window).transpose(1, 0, 2), exmat), axis=1)
-    exmat = np.concatenate((exmat, np.array([exmat[:, -1]] * window).transpose(1, 0, 2)), axis=1)
-
-    filter = np.ones((fsize, fsize, 3), np.float) / fsize ** 2
-    tmp_m = np.zeros_like(mat, np.float)
+        return self.targeted(tmpm, target)
     
-    for x, y in product(range(window, X + window), range(window, Y + window)):
-        u, d, l, r = y - window, y + window + 1, x - window, x + window + 1
-        tmp_m[y - window, x - window] = sum(sum(filter * exmat[u:d, l:r]))
+    def median(self, window, target):
+        tmpm = np.zeros_like(self.mat, np.float)
+        for x, y in product(range(window, self.X + window), range(window, self.Y + window)):
+            u, d, l, r = y - window, y + window + 1, x - window, x + window + 1
+            tmpm[y - window, x - window] = np.median(self.extend_mat(window)[u:d, l:r])
 
-    print(tmp_m)
+        return self.targeted(tmpm, target)
 
-    if 'all' in terget:
-        return tmp_m
+    def targeted(self, retm, target):
+        retmat = self.mat.copy()
+        if 'all' in target or target is None:
+            return retm
 
-    if 'Y' in terget:
-        retmat[:, :, 0] = tmp_m[:, :, 0]
+        if 'Y' in target:
+            retmat[:, :, 0] = retm[:, :, 0]
+        if 'Cb' in target:
+            retmat[:, :, 1] = retm[:, :, 1]
+        if 'Cr' in target:
+            retmat[:, :, 2] = retm[:, :, 2]
+
+        return retmat
+
     
-    if 'Cb' in terget:
-        retmat[:, :, 1] = tmp_m[:, :, 1]
-    
-    if 'Cr' in terget:
-        retmat[:, :, 2] = tmp_m[:, :, 2]
 
-    return retmat
+# def conv(mat, mode, window, terget):
+#     retmat = mat.copy()
+#     exmat = mat.copy()
+#     X, Y = mat.shape[:-1]
+#     fsize = 1 + 2 * window
+#     print(f'using {fsize} * {fsize} filter')
+
+#     exmat = np.concatenate(([exmat[0]] * window, exmat), axis=0)
+#     exmat = np.concatenate((exmat, [exmat[-1]] * window), axis=0)
+#     exmat = np.concatenate((np.array([exmat[:, 0]] * window).transpose(1, 0, 2), exmat), axis=1)
+#     exmat = np.concatenate((exmat, np.array([exmat[:, -1]] * window).transpose(1, 0, 2)), axis=1)
+
+#     filter = np.ones((fsize, fsize, 3), np.float) / fsize ** 2
+#     tmp_m = np.zeros_like(mat, np.float)
+    
+#     for x, y in product(range(window, X + window), range(window, Y + window)):
+#         u, d, l, r = y - window, y + window + 1, x - window, x + window + 1
+#         tmp_m[y - window, x - window] = sum(sum(filter * exmat[u:d, l:r]))
+
+#     print(tmp_m)
+
+#     if 'all' in terget:
+#         return tmp_m
+
+#     if 'Y' in terget:
+#         retmat[:, :, 0] = tmp_m[:, :, 0]
+    
+#     if 'Cb' in terget:
+#         retmat[:, :, 1] = tmp_m[:, :, 1]
+    
+#     if 'Cr' in terget:
+#         retmat[:, :, 2] = tmp_m[:, :, 2]
+
+#     return retmat
 
 
 def RGBtoYCC(RGB):
--- main.py
+++ main.py
@@ -3,16 +3,9 @@ from digital_library import *
 np.set_printoptions(100)
 
 if __name__ == '__main__':
-    RGB = loadimg('lennaN.bmp')
-    showimg(RGB)
-    YCC = RGBtoYCC(RGB)
-    YCC = conv(YCC, 'average', 2, 'all')
-    showimg(YCCtoRGB(YCC))
-        
-    #YCC1 = conv(YCC, 1, 'Y')
+    filter = filter(1)
+    RGB = filter2D(loadimg('lennaN.bmp'))
+    
+    
 
-    #showimg(YCC1)
 
-    #showimg(YCCtoRGB(YCC1))
-    # nshowimg(YCCtoRGB(YCC2))
-    # showimg(YCCtoRGB(YCC3))
